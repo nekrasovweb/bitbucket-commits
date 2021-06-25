193a65375d4ce94d7004a76e1881581d57fa30f5
nekrasovweb <nekrasovweb@gmail.com>
2021-06-25 18:33:33 +0300

    edit format output

--- commits.py
+++ commits.py
@@ -1,63 +1,52 @@
 #!/usr/local/bin/python
 
-import json, requests, dateutil.parser, datetime
+import os, json, requests, datetime, urllib3
 from collections import defaultdict
 
-username = "user"
-password = "password"
-baseUrlv1 = "http://bitbucket-server-hostname/rest/api/1.0"
-year = 2017
-
-def getAllItems(baseUrl):
-	allItems = []
-	r = requests.get(baseUrl, auth=(username,password))
-	c = r.json()
-	if 'values' not in c:
-		print c #debug
-
-	allItems.extend(c['values'])
-	while not c['isLastPage']:
-		r = requests.get("{base}?start={nextPageStart}".format(base=baseUrl, 
-			nextPageStart=c['nextPageStart']),
-			auth=(username,password))
-		c = r.json()
-		allItems.extend(c['values'])
-
-	return allItems
-
-def iterateAllCommitsForRepo(key, slug):
-	allCommits = getAllItems("{base}/projects/{key}/repos/{slug}/commits".
-		format(base=baseUrlv1,key=key,slug=slug))
-
-	for commit in allCommits:
-		#print commit
-		commitDate = datetime.datetime.fromtimestamp(commit['authorTimestamp']/1000)
-		print commitDate
-		print commitDate.year
-		if commitDate.year == year:			
-			name = commit['author']['name']
-			d[name] += 1
+username = os.environ.get('BB_USER')
+password = os.environ.get('BB_PASS')
+base_api_url = os.environ.get('BB_API_URL')
+path_root_ca = os.environ.get('BB_PATH_CA')
+year = 2021
 
 
+def get_all_items(base_url):
+    all_items = []
+    r = requests.get(base_url, auth=(username, password), verify=path_root_ca)
+    c = r.json()
+    if 'values' not in c:
+        print(c)  # debug
 
-def iterateRepositoriesForProject(key):
-	allRepositories = getAllItems("{base}/projects/{key}/repos".format(base=baseUrlv1,key=key))
-	for repository in allRepositories:
-		print repository['slug']
-		iterateAllCommitsForRepo(key, repository['slug'])
+    all_items.extend(c['values'])
+    while not c['isLastPage']:
+        r = requests.get("{base}?start={nextPageStart}".format(
+            base=base_url,
+            nextPageStart=c['nextPageStart']),
+            auth=(username, password),
+            verify=path_root_ca)
+        c = r.json()
+        all_items.extend(c['values'])
 
-def iterateAllProjects():
-	allProjects = getAllItems("{base}/projects".format(base=baseUrlv1))
-	for project in allProjects:
-		iterateRepositoriesForProject(project['key'])
-
-
-d = defaultdict(int)
-iterateAllProjects()
-print d
+    return all_items
 
 
+def it_commits_for_repo(key, slug):
+    all_commits = get_all_items("{base}/projects/{key}/repos/{slug}/commits".
+                                format(base=base_api_url, key=key, slug=slug))
+    print('project key:', key, 'repo_name:', slug, 'size_commit:', all_commits.__len__())
+
+
+def it_repositories_for_project(key):
+    all_repositories = get_all_items("{base}/projects/{key}/repos".format(base=base_api_url, key=key))
+    for repository in all_repositories:
+        it_commits_for_repo(key, repository['slug'])
+
+
+def it_projects():
+    all_projects = get_all_items("{base}/projects".format(base=base_api_url))
+    for project in all_projects:
+        it_repositories_for_project(project['key'])
+
+
+it_projects()
 quit()
-
-	
-
